034b168eeac1dec57b27c6f0b67c98d144a3fb27
Aaron Summers <aaron.summers2797@gmail.com>
2019-07-28 17:00:12 +0100

    minor text changes

---
+++ server-side/app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,26 @@
+class Api::V1::UsersController < ApplicationController
+
+    # skip_before_action :authorize, only: [:create]
+
+    def index
+        users = User.all
+
+        render json: users
+    end
+
+
+    def create
+        user = User.create( user_params )
+        if user.valid?
+            render json: { user: UserSerializer.new(user), token: issue_token(user_id: user.id) }, status: :created
+        else
+            render json: { errors: user.errors.full_messages }, status: :not_accepted
+        end
+    end
+
+    private 
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+end
